3044f9fbd2326f9637fe184cc5e6d6941d51db8d
Jeremy Fowler <jembyfowler@gmail.com>
2021-11-17 16:36:27 -0700

    test

--- Monsters.client/src/pages/HomePage.vue
+++ Monsters.client/src/pages/HomePage.vue
@@ -1,29 +1,26 @@
 <template>
   <div class="container">
     <div class="row">
-      <div class="component">
-        <button class="btn btn-primary" @click.prevent="getMonsters()">
-          Get Monsters
-        </button>
-        <button
-          v-if="nextPage"
-          class="btn btn-primary"
-          @click.prevent="changePage(nextPage)"
-        >
-          next page
-        </button>
-        <button
-          v-if="previousPage"
-          class="btn btn-primary"
-          @click.prevent="changePage(previousPage)"
-        >
-          previous page
-        </button>
-        <h1>Yo</h1>
-      </div>
+      <button class="btn btn-primary" @click.prevent="getMonsters()">
+        Get Monsters
+      </button>
+      <button
+        v-if="nextPage"
+        class="btn btn-primary"
+        @click.prevent="changePage(nextPage)"
+      >
+        next page
+      </button>
+      <button
+        v-if="previousPage"
+        class="btn btn-primary"
+        @click.prevent="changePage(previousPage)"
+      >
+        previous page
+      </button>
     </div>
     <div class="row">
-      <MonsterCard v-for="m in monsters" :key="m.name" :monster="m" />
+      <MonsterCard v-for="m in monsters" :key="m.slug" :monster="m" />
     </div>
   </div>
 </template>
@@ -34,19 +31,20 @@ import { computed } from "@vue/reactivity"
 import { monstersService } from "../services/MonstersService"
 import Pop from "../utils/Pop"
 import { AppState } from "../AppState"
+import { watchEffect } from '@vue/runtime-core'
 export default {
   setup() {
+    watchEffect(async () => {
+      try {
+        await monstersService.getMonsters()
+      } catch (error) {
+        Pop.toast(error, 'error')
+      }
+    })
     return {
       monsters: computed(() => AppState.monsters),
       previousPage: computed(() => AppState.previousPage),
       nextPage: computed(() => AppState.nextPage),
-      async getMonsters() {
-        try {
-          await monstersService.getMonsters()
-        } catch (error) {
-          Pop.toast(error, 'error')
-        }
-      },
       async changePage(page) {
         try {
           await monstersService.changePage(page)
