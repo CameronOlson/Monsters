56657e979a5f82aec1135ad5157b2663d1d23bea
Cameron Olson <olsoncam10@gmail.com>
2021-11-25 21:25:36 -0700

    can sort by CR rating but need to reset th app state

--- Monsters.client/src/pages/HomePage.vue
+++ Monsters.client/src/pages/HomePage.vue
@@ -10,10 +10,14 @@
       <button class="m-1 btn btn-primary" @click.prevent="zeroFilter()">
         "0 - 1"
       </button>
-      <button class="m-1 btn btn-primary">2 - 5</button>
-      <button class="m-1 btn btn-primary">6 - 10</button>
-      <button class="m-1 btn btn-primary">10 - 15</button>
-      <button class="m-1 btn btn-primary">15 - 20</button>
+      <button class="m-1 btn btn-primary" @click="fiveFilter()">2 - 5</button>
+      <button class="m-1 btn btn-primary" @click="tenFilter()">6 - 10</button>
+      <button class="m-1 btn btn-primary" @click="fifteenFilter()">
+        10 - 15
+      </button>
+      <button class="m-1 btn btn-primary" @click="twentyFilter()">
+        15 - 20
+      </button>
 
       <!-- <button class="m-1 btn btn-primary" @click="lowFilter = !lowFilter">
         ScoreSorter
@@ -63,6 +67,26 @@ export default {
       },
       zeroFilter() {
         const result = AppState.monsters.filter(m => m.challenge_rating === "1/4" || m.challenge_rating === "1/2" || m.challenge_rating === "1")
+        console.log(result)
+        AppState.monsters = result
+      },
+      fiveFilter() {
+        const result = AppState.monsters.filter(m => m.challenge_rating === "2" || m.challenge_rating === "3" || m.challenge_rating === "4" || m.challenge_rating === "5")
+        console.log(result)
+        AppState.monsters = result
+      },
+      tenFilter() {
+        const result = AppState.monsters.filter(m => m.challenge_rating === "6" || m.challenge_rating === "7" || m.challenge_rating === "8" || m.challenge_rating === "9" || m.challenge_rating === "10")
+        console.log(result)
+        AppState.monsters = result
+      },
+      fifteenFilter() {
+        const result = AppState.monsters.filter(m => m.challenge_rating === "11" || m.challenge_rating === "12" || m.challenge_rating === "13" || m.challenge_rating === "14" || m.challenge_rating === "15")
+        console.log(result)
+        AppState.monsters = result
+      },
+      twentyFilter() {
+        const result = AppState.monsters.filter(m => m.challenge_rating === "16" || m.challenge_rating === "17" || m.challenge_rating === "18" || m.challenge_rating === "19" || m.challenge_rating === "20")
         console.log(result)
         AppState.monsters = result
       },
