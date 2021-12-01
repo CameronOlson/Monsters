837ec75e44df3094bf934da70e1bb76c387f95d7
Cameron Olson <olsoncam10@gmail.com>
2021-12-01 16:39:51 -0700

    did some work on the monster modal

--- Monsters.client/src/components/Modal.vue
+++ Monsters.client/src/components/Modal.vue
@@ -5,7 +5,7 @@
     aria-labelledby="exampleModalLabel"
     aria-hidden="true"
   >
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-xl">
       <div class="modal-content">
         <div class="modal-header">
           <h5 class="modal-title" id="exampleModalLabel">
--- Monsters.client/src/components/MonsterCard.vue
+++ Monsters.client/src/components/MonsterCard.vue
@@ -15,13 +15,53 @@
   </button>
   <Modal :id="'monster' + monster.slug">
     <template #modal-title>
-      <h6>This is your monster</h6>
-    </template>
-    <template #modal-body>
       <h1>{{ monster.name }}</h1>
-      <div>AC:{{ monster.armor_class }}</div>
-      <div>HP: {{ monster.hit_points }}</div>
-      <div>CR:{{ monster.challenge_rating }}</div>
+      <div>CR:{{ monster.challenge_rating }}</div></template
+    >
+    <template #modal-body>
+      <div class="row">
+        <div class="col-6">
+          <div class="row">
+            <div class="col-6">
+              <div>AC:{{ monster.armor_class }}</div>
+            </div>
+            <div class="col-6">
+              <div>HP: {{ monster.hit_points }}</div>
+            </div>
+            <div class="col-12">
+              <div class="row">
+                <div class="col-2">STR {{ monster.strength }}</div>
+                <div class="col-2">DEX {{ monster.dexterity }}</div>
+                <div class="col-2">CON {{ monster.constitution }}</div>
+                <div class="col-2">INT {{ monster.intelligence }}</div>
+                <div class="col-2">WIS {{ monster.wisdom }}</div>
+                <div class="col-2">CHA {{ monster.charisma }}</div>
+                <div class="col-12">
+                  <strong> Damage Immunities </strong> <br />
+                  {{ monster.damage_immunities }}
+                </div>
+                <div class="col-12">
+                  <strong>Conditional Immunities</strong> <br />
+                  {{ monster.condition_immunities }}
+                </div>
+                <div class="col-12">
+                  <strong>Senses</strong> <br />
+                  {{ monster.senses }}
+                </div>
+                <div class="col-12">
+                  <strong>Languages</strong> <br />
+                  {{ monster.languages }}
+                </div>
+                <div class="col-12">
+                  <strong>Actions</strong> <br />
+                  {{ monster.actions }}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-6">Trascore</div>
+      </div>
     </template>
   </Modal>
 </template>
