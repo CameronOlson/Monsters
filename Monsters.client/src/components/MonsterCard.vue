99bca507afec71633c9f663f74f20e51bb70edde
Cameron Olson <olsoncam10@gmail.com>
2021-12-06 14:07:05 -0700

    changed the buttons on home screen

--- Monsters.client/src/components/MonsterCard.vue
+++ Monsters.client/src/components/MonsterCard.vue
@@ -15,84 +15,110 @@
     </div>
   </button>
   <Modal :id="'monster' + monster.slug">
-    <template #modal-title>
-      <h1>{{ monster.name }}</h1>
-      <div>CR:{{ monster.challenge_rating }}</div></template
-    >
+    <template #modal-title class="monster-card">
+      <div>
+        <div>
+          <h1 style="font-family: kings">{{ monster.name }}</h1>
+        </div>
+        <div>CR:{{ monster.challenge_rating }}</div>
+      </div>
+    </template>
     <template #modal-body>
       <div class="row">
         <div class="col-6">
           <div class="row">
+            <div class="col-12 d-flex">
+              <div>{{ monster.type }}</div>
+              <div class="ps-2">{{ monster.subtype }}</div>
+            </div>
+          </div>
+          <div class="row">
             <div class="col-6">
-              <div>AC:{{ monster.armor_class }}</div>
+              <div><strong> AC: </strong> {{ monster.armor_class }}</div>
             </div>
             <div class="col-6">
-              <div>HP: {{ monster.hit_points }}</div>
+              <div><strong>HP:</strong> {{ monster.hit_points }}</div>
+              <!-- <div>Speed: {{ monster.speed }}</div> -->
             </div>
             <div class="col-12">
               <div class="row">
-                <div class="col-2">STR {{ monster.strength }}</div>
-                <div class="col-2">DEX {{ monster.dexterity }}</div>
-                <div class="col-2">CON {{ monster.constitution }}</div>
-                <div class="col-2">INT {{ monster.intelligence }}</div>
-                <div class="col-2">WIS {{ monster.wisdom }}</div>
-                <div class="col-2">CHA {{ monster.charisma }}</div>
-                <div class="col-12">
-                  <div v-if="monster.damage_immunities == ''"></div>
-                  <div v-else>
-                    <strong> Damage Immunities </strong> <br />
-                    {{ monster.damage_immunities }}
-                  </div>
+                <div class="col-2">
+                  <strong><u>STR</u></strong> {{ monster.strength }}
                 </div>
-                <div class="col-12">
-                  <div v-if="monster.damage_resistances == ''"></div>
-                  <div v-else>
-                    <strong>Damage Resistance</strong> <br />
-                    {{ monster.damage_resistances }}
-                  </div>
+                <div class="col-2">
+                  <strong><u>DEX</u></strong> {{ monster.dexterity }}
                 </div>
-                <div class="col-12">
-                  <div v-if="monster.damage_vulnerabilities == ''"></div>
-                  <div v-else>
-                    <strong>Damage Vulnerabilities</strong> <br />
-                    {{ monster.damage_vulnerabilities }}
-                  </div>
+                <div class="col-2">
+                  <strong><u>CON</u></strong> {{ monster.constitution }}
                 </div>
-                <div class="col-12">
-                  <div v-if="monster.condition_immunities == ''"></div>
-                  <div v-else>
-                    <strong>Conditional Immunities</strong> <br />
-                    {{ monster.condition_immunities }}
-                  </div>
+                <div class="col-2">
+                  <strong><u>INT</u></strong> {{ monster.intelligence }}
                 </div>
-                <div class="col-12">
-                  <strong>Senses</strong> <br />
-                  {{ monster.senses }}
+                <div class="col-2">
+                  <strong><u>WIS</u></strong> {{ monster.wisdom }}
                 </div>
-                <div class="col-12">
-                  <strong>Languages</strong> <br />
-                  {{ monster.languages }}
+                <div class="col-2">
+                  <strong><u>CHA</u></strong> {{ monster.charisma }}
                 </div>
-                <div class="col-12">
-                  <div>
-                    <strong> Spells </strong>
-                  </div>
-                  <br />
-                  <div>
-                    <div v-for="spells in spells" :key="spells.name">
-                      <div class="card">
-                        <div class="card-body">
-                          <div class="card-header">
-                            <b>{{ spells.name }}</b>
-                          </div>
-                          <div>Components: {{ spells.components }}</div>
-                          <div>Concentration: {{ spells.concentration }}</div>
-                          <div>Casting Time: {{ spells.casting_time }}</div>
-                          <div>Duration: {{ spells.duration }}</div>
-                          <div>Range: {{ spells.range }}</div>
-                          <div>
-                            {{ spells.desc }}
-                          </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="row">
+                <div class="col-12"></div>
+                <div v-if="monster.damage_immunities == ''"></div>
+                <div v-else>
+                  <strong> Damage Immunities </strong> <br />
+                  {{ monster.damage_immunities }}
+                </div>
+              </div>
+              <div class="col-12">
+                <div v-if="monster.damage_resistances == ''"></div>
+                <div v-else>
+                  <strong>Damage Resistance</strong> <br />
+                  {{ monster.damage_resistances }}
+                </div>
+              </div>
+              <div class="col-12">
+                <div v-if="monster.damage_vulnerabilities == ''"></div>
+                <div v-else>
+                  <strong>Damage Vulnerabilities</strong> <br />
+                  {{ monster.damage_vulnerabilities }}
+                </div>
+              </div>
+              <div class="col-12">
+                <div v-if="monster.condition_immunities == ''"></div>
+                <div v-else>
+                  <strong>Conditional Immunities</strong> <br />
+                  {{ monster.condition_immunities }}
+                </div>
+              </div>
+              <div class="col-12">
+                <strong>Senses</strong> <br />
+                {{ monster.senses }}
+              </div>
+              <div class="col-12">
+                <strong>Languages</strong> <br />
+                {{ monster.languages }}
+              </div>
+              <div class="col-12">
+                <div>
+                  <strong> Spells </strong>
+                </div>
+                <br />
+                <div>
+                  <div v-for="spells in spells" :key="spells.name">
+                    <div class="card">
+                      <div class="card-body">
+                        <div class="card-header">
+                          <b>{{ spells.name }}</b>
+                        </div>
+                        <div>Components: {{ spells.components }}</div>
+                        <div>Concentration: {{ spells.concentration }}</div>
+                        <div>Casting Time: {{ spells.casting_time }}</div>
+                        <div>Duration: {{ spells.duration }}</div>
+                        <div>Range: {{ spells.range }}</div>
+                        <div>
+                          {{ spells.desc }}
                         </div>
                       </div>
                     </div>
@@ -118,6 +144,7 @@
                 <b>{{ actions.name }}</b> - {{ actions.desc }}
               </div>
               <strong>Legendary Actions</strong> <br />
+              <p>{{ monster.legendary_desc }}</p>
               <div
                 v-for="legendary_actions in monster.legendary_actions"
                 :key="legendary_actions.name"
@@ -183,5 +210,9 @@ export default {
 .monster-card {
   display: flex;
   justify-content: space-between;
+}
+.monster-card2 {
+  display: flex;
+  justify-content: space-around;
 }
 </style>
